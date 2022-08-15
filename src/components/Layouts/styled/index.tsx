9b95d913f07b10f75566e967ce628119c07dbbb8
Nolann Biron <biron.nolann@gmail.com>
2022-08-15 17:08:04 +0200

    Update icon

--- src/components/Layouts/styled/index.tsx
+++ src/components/Layouts/styled/index.tsx
@@ -16,7 +16,13 @@ export const HeaderWrapper = chakra('header', {
 
 export const LogoWrapper = ({ name }: { name: string }) => (
     <Flex alignItems="center" gap={3}>
-        <Logo />
+        {/* <Logo /> */}
+        <img
+            src="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🖖</text></svg>"
+            width="30px"
+            height="30px"
+            alt=""
+        />
         <Heading display={['none', '', 'block']} as="h1" size="md" textTransform="uppercase">
             {name}
         </Heading>
